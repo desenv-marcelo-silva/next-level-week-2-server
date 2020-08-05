2ba2a462c6c8cda9de54940583708eee8560d594
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-08-05 00:57:05 -0300

    feat(classes): list classes and custom filters

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -10,6 +10,33 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+  async index(req: Request, res: Response) {
+    const filters = req.query;
+
+    if (!filters.subject || !filters.week_day || !filters.time) {
+      return res.status(400).json({ error: 'Missing parameters' });
+    }
+
+    const timeInMinutes = convertHourToMinuts(filters.time as string);
+
+    const classes = await db('classes')
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [
+            Number(filters.week_day),
+          ])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
+      })
+      .where('classes.subject', '=', filters.subject as string)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*']);
+
+    return res.json({ classes });
+  }
+
   async create(req: Request, res: Response) {
     const { name, avatar, whatsapp, bio, subject, cost, schedule } = req.body;
 
